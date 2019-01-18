8946a05b65055f9bf959f123e3e428d77cacd3c6
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-01-18 10:03:11 -0600

    commit

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="header">
       <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times!!!!!!!!</h1>
+      <h1>Lambda Times!!!!!!!!!!!!</h1>
       <span className="temp">98°</span>
     </div>
   )
