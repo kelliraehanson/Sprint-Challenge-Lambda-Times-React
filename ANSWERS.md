eaee8a906a1658c47fcf326ea08dba88eb0465c7
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-01-18 13:02:28 -0600

    commit after my computer started working again

---
+++ ANSWERS.md
@@ -0,0 +1,20 @@
+
+
+
+
+1. PropTypes is a way of making runtime is a way of making it known the data of a React component requiers in order to render properly. 
+It helps with bugs.
+
+2. Every component has a lifecycle and React has methods that get called at each point in their lifecycles that gives you control over what happens.
+
+3. A higher-order component is a function that takes a component and returns a new component.
+
+4. 
+
+- CSS Stylesheet: Simply import css file import so you can have a separate css file for each component.
+
+
+- Inline styling: Styling that is specified with an object whose key is the camelCased version of the style name, and whose value is the style’s value.
+
+
+- CSS Modules: A CSS Module is a CSS file in which all class names and animation names are scoped locally by default. 
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
-      if it is not it should just be 'tab'*/
+ 
+
+      let kelli = '';
+
+      if(props.tab === props.selectedTab) {
+        kelli = 'tab active-tab';
+      } else {
+        kelli = "tab";
+      };
+
+
+
   return (
+    
     <div
-      className={''}
+      className={kelli}
       onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
+
+        const tab = props.tab;
+
+        props.selectTabHandler(tab);
+       
       }}
     >
       {props.tab.toUpperCase()}
@@ -17,6 +31,11 @@ const Tab = props => {
   );
 };
 
-// Make sure you include PropTypes on your props.
+
+Tab.propTypes = {
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.array,
+  tab: PropTypes.array,
+}
 
 export default Tab;
